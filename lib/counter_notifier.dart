6104b9d15d625e3f929ab023169e72e6f1ba27f3
Jordan Holland <jordan.mark.holland@gmail.com>
2021-08-11 10:25:02 +1000

    First commit.

---
+++ lib/counter_notifier.dart
@@ -0,0 +1,14 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_counter/counter_state.dart';
+
+final counterProvider =
+    StateNotifierProvider.autoDispose<CounterNotifier, CounterState>(
+  (_) => CounterNotifier(),
+);
+
+class CounterNotifier extends StateNotifier<CounterState> {
+  CounterNotifier() : super(CounterState());
+
+  void increase() => state = state.copyWith(count: state.value + 1);
+  void decrease() => state = state.copyWith(count: state.value - 1);
+}
